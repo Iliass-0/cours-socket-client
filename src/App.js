b05231453825490f8b7aa2d233b2196662f8d717
Chris Crognier <christophe.crognier@gmail.com>
2023-07-03 16:37:05 +0200

    Send message to server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import io from 'socket.io-client';
+import {useEffect, useState} from 'react';
 
 const socket = io.connect('http://localhost:3001');
 
 function App() {
+
+  // Ce state me permet de définir les valeur que je vais utiliser dans mon composant
+  // Ici on définis le message que l'on va récupérer de L'INPUT
+  const [message, setMessage] = useState("");
+  // Ici on définis le message RECU par le serveur
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", {message});
+  };
+
+  // Le useEffect va s'activer lorsque le module "socket" est modifié
+  // Dans notre cas, c'est lorsque on reçoit un message du serveur
+  // useEffect(() => {
+  //   socket.on("receive_message", (data) => {
+
+  //   });
+  // }, [socket]);
+
   return (
     <div className="App">
-      Hello la mif
+      <input placeholder="Message..." onChange={(event) => {
+        setMessage(event.target.value);
+      }}/>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
